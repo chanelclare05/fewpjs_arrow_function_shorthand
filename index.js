761af905048170dc3b6a78e2872468338c2e4468
Chanel Donahoe <cdonahoe05@gmail.com>
2021-07-14 19:00:34 +1000

    completed lab

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+let divide = (num1, num2) => {
+    if (num2 > 0) {
+    return num1/num2;
+    } else {return 20}
+}
+const num1 = 2000
+const num2 = 100
+
+let square = (num1) => num1 * num1;
+
+let add = (num1, num2) => num1 + num2;
